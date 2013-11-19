2717557866044f0daadbf0b04a91f5092aa0d9fc
Tron Thongsringklee <tron@bashton.com>
2013-11-19 13:46:09 +0000

    Add random files generator

--- generate-random-files.sh
+++ generate-random-files.sh
@@ -1,1 +1,13 @@
-/home/tron/dev/tron/example/bash/generate-random-files.sh
+#!/bin/bash
+
+if [ $# -ne '2' ]; then
+  echo "Usage: $0 <amount of files> <size in Byte>"
+  exit 1
+fi
+
+for (( c=1; c<=$1; c++ ))
+do
+	dd if=/dev/zero of=file$c bs=$2 count=1
+done
+ 
+exit 0
